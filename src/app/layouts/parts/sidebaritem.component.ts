df8051df73be6e26ac11820ebecec9403cf33395
unknown <bhimpdsharma@gmail.com>
2022-11-30 10:14:08 +0545

    fixed multilevel sidebar menu

--- src/app/layouts/parts/sidebaritem.component.ts
+++ src/app/layouts/parts/sidebaritem.component.ts
@@ -3,14 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebaritem',
   template: `<li class="nav-item" *ngFor="let mit of menuItems">
-  <a class="nav-link collapsed" (click)="isCollapsed = !isCollapsed" href="javascript:void(0)" *ngIf="hasSubMenu(mit)">
-      <i class="{{mit.icon}}"></i>
-      <span>{{mit.title}}</span>
-      <i class="bi bi-chevron-down ms-auto" [ngClass]="isCollapsed?'bi-chevron-down':'bi-chevron-up'"></i>
-    </a>
-    <ul class="nav-content" [collapse]="isCollapsed" [isAnimated]="true">
-      <app-sidebaritem [menuItems]="mit.childs" [isChildLink]="true"></app-sidebaritem>
-    </ul>
+    <app-sidebaritem-inner *ngIf="hasSubMenu(mit)" [menuItem]="mit"></app-sidebaritem-inner>
     <a [ngClass]="!isChildLink?'nav-link collapsed':''" [routerLink]="mit.link" *ngIf="!hasSubMenu(mit)">
           <i class="{{mit.icon}}"></i>
           <span>{{mit.title}}</span>
@@ -20,7 +13,6 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SidebaritemComponent implements OnInit {
   @Input("menuItems") menuItems:any;
   @Input("isChildLink") isChildLink?=false;
-  isCollapsed = true;
   active = false;
   constructor() { }
 

025f8215aa0c3002084453fc1c88551a80415bb5
unknown <bhimpdsharma@gmail.com>
2025-04-03 23:44:32 +0545

    removed standalone flag as set as default in angualr 19

--- src/app/blank-page/blank-page.component.ts
+++ src/app/blank-page/blank-page.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-blank-page',
     templateUrl: './blank-page.component.html',
-    styleUrls: ['./blank-page.component.scss'],
-    standalone: true
+    styleUrls: ['./blank-page.component.scss']
 })
 export class BlankPageComponent implements OnInit {
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
-    standalone: true
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
